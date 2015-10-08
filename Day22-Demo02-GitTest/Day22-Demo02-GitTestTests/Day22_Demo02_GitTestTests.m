4e36cd96fda752a4e82576a1496c31774ba69d99
tarena13 <tarena13@tarena17deiMac.local>
2015-10-08 11:56:02 +0800

    test1

---
+++ Day22-Demo02-GitTest/Day22-Demo02-GitTestTests/Day22_Demo02_GitTestTests.m
@@ -0,0 +1,40 @@
+//
+//  Day22_Demo02_GitTestTests.m
+//  Day22-Demo02-GitTestTests
+//
+//  Created by tarena13 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Day22_Demo02_GitTestTests : XCTestCase
+
+@end
+
+@implementation Day22_Demo02_GitTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
